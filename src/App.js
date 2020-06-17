60c97eb93e093144f37344d9b3401830612b5427
Fabio Muller <fabioml10@yahoo.com.br>
2020-06-17 12:46:41 -0300

    Fetch API. Send info to card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
-import ProjetoBase from './components/ProjetoBase/ProjetoBase';
+import Card from './components/Card/Card';
 
 export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      votes: []
+    }
+  }
+
+  async componentDidMount() {
+    await this.fetchData()
+    // setInterval(() => this.fetchData(), 3000);
+  }
+
+  async fetchData() {
+    const res = await fetch('http://localhost:8080/votes')
+    const json = await res.json()
+
+    this.setState({
+      votes: json.candidates
+    })
+  }
+
   render() {
-    return <ProjetoBase />;
+    const { votes } = this.state
+    return (
+      <div className="container">
+        <h1>Votação</h1>
+        <Card votes={votes} />
+      </div>
+    )
   }
 }
